9efd1e70054fed4cba2256ad7987a03aa6d6c825
Jithin J Kumar <jithinjkumar@gmail.com>
2019-04-09 21:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ import time
 time_now = datetime.datetime.now()
 
 delta = int(input("Enter time delta in seconds between 10 and 20 seconds :"))
-print("Current time is ",time_now)
-endtime = time_now+datetime.timedelta(seconds=delta)
-time.sleep(delta/2)
-print("beep")
-time.sleep(delta/2)
-print("beep",endtime)
+if 10 < delta < 20:
+  print("Current time is ",time_now)
+  endtime = time_now+datetime.timedelta(seconds=delta)
+  time.sleep(delta/2)
+  print("Half time : beep")
+  time.sleep(delta/2)
+  print("Full Time : beep") 
+else:
+  print("invalid Entry")
+  
